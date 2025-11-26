b02e45bc61b3be47094f3ae1ab581cc678fa3e1f
gabrielecantatore05-png <gabriele.cantatore05@gmail.com>
2025-11-26 10:44:34 +0100

    Implementazione logica client-server TCP

--- server-project/src/main.c
+++ server-project/src/main.c
@@ -22,57 +22,127 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "protocol.h"
 
-#define NO_ERROR 0
+void errorhandler(char *errorMessage){
+	printf("%s\n", errorMessage);
+}
 
-void clearwinsock() {
+void clearwinsock(){
 #if defined WIN32
-	WSACleanup();
+        WSACleanup();
 #endif
 }
 
-int main(int argc, char *argv[]) {
-
-	// TODO: Implement server logic
+int main(int argc, char *argv[]){
 
 #if defined WIN32
-	// Initialize Winsock
-	WSADATA wsa_data;
-	int result = WSAStartup(MAKEWORD(2,2), &wsa_data);
-	if (result != NO_ERROR) {
-		printf("Error at WSAStartup()\n");
-		return 0;
-	}
+        WSDATA wsa_data;
+		if(WSAStartup(MAKEWORD(2,2), &wsa_data) !=0){
+			printf("Errore in WSAStartup()\n");
+			return 0;
+		}
 #endif
+        int port = SERVER_PORT;
+		char ip[32] = DEFAULT_IP;
+		char request_string[128] = "";
+		int richiesta =0;
 
-	int my_socket;
+		weather_request_t request;
+	memset(&request, 0, sizeof(request));
 
-	// TODO: Create socket
-	// my_socket = socket(...);
 
-	// TODO: Configure server address
-	// struct sockaddr_in server_addr;
-	// server_addr.sin_family = AF_INET;
-	// server_addr.sin_port = htons(SERVER_PORT);
-	// server_addr.sin_addr.s_addr = INADDR_ANY;
+	for (int i =1; i< argc; i++) {
+		     if (strcmp(argv[i], "-s") ==0 && i+1 < argc){
+				    strncpy(ip, argv[i++], 31);
+			 }else if (strcmp(argv[i], "-p") == 0 && i+1 < argc) {
+				    port = atoi(argv[++i]);
+			 }else if (strcmp(argv[i], "-r") == 0 && i + 1 < argc) {
+				    strncpy(request_string, argv[i++], 127);
+					richiesta = 1;
+	}        
+}
 
-	// TODO: Bind socket
-	// bind(...);
+if (!richiesta) {
+	    printf("Uso: %s [-s server] [-p port] -r \"tipo città\"\n", argv[0]);
+		clearwinsock();
+		return -1;
+}
 
-	// TODO: Set socket to listen
-	// listen(...);
+int i = 0;
+while (request_string[i] == ' ' && request_string[i] != '\0') i++;
 
-	// TODO: Implement connection acceptance loop
-	// while (1) {
-	//     int client_socket = accept(...);
-	//     // Handle client communication
-	//     closesocket(client_socket);
-	// }
+request.type = request_string[i];
+i++;
 
-	printf("Server terminated.\n");
+while (request_string[i] == ' ' && request_string[i] != '\0') i++;
 
-	closesocket(my_socket);
+
+strncpy(request.city, &request_string[i], 63);
+
+
+int c_socket = socket( PF_INET, SOCK_STREAM, IPPROTO_TCP);
+if (c_socket < 0) {
+	    errorhandler("Creazione della socket fallita.\n");
+		closesocket(c_socket);
+		clearwinsock();
+        return -1;
+}
+
+struct sockaddr_in sad;
+	memset(&sad, 0, sizeof(sad));
+	sad.sin_family = AF_INET;
+	sad.sin_addr.s_addr = inet_addr(ip);
+	sad.sin_port = htons(port);
+
+if (connect(c_socket, (struct sockaddr *)&sad, sizeof(sad)) < 0) {
+		errorhandler("Connessione fallita.\n");
+		closesocket(c_socket);
+		clearwinsock();
+		return -1;
+	}
+
+
+
+if (send(c_socket, (char*)&request, sizeof(request), 0) != sizeof(request)) {
+	      errorhandler("send() ha inviato un numero di byte diverso dall'atteso");
+		  closesocket(c_socket);
+		  clearwinsock();
+		  return -1;
+}
+
+
+weather_response_t response;
+	if ((recv(c_socket, (char*)&response, sizeof(response), 0)) <= 0) {
+		errorhandler("recv() fallita o connessione chiusa prematuramente");
+		closesocket(c_socket);
+		clearwinsock();
+		return -1;
+	}
+
+
+     printf("Ricevuto risultato dal server ip %s. ", ip);
+
+	 if (response.status == 0) {
+		     request.city[0] = toupper(request.city[0]);
+			 switch (response.type) {
+				     case 't' : printf("%s: Temperatura = %1.f°C\n", request.city,response.value); break;
+					 case 'h' : printf("%s: Umidità = %.1f%%\n", request.city, response.value); break;
+			         case 'w': printf("%s: Vento = %.1f km/h\n", request.city, response.value); break;
+			         case 'p': printf("%s: Pressione = %.1f hPa\n", request.city, response.value); break;
+			         default:  printf("Tipo sconosciuto ricevuto.\n");
+			 }
+    } else if (response.status == 1) {
+		    printf("Città non disponibile\n");
+	} else if (response.status == 2) {
+		    printf("Richiesta non valida\n");
+	} else {
+		    printf("Errore sconosciuto\n");
+	}
+
+	closesocket(c_socket);
+
 	clearwinsock();
 	return 0;
-} // main end
+}
--- server-project/src/protocol.h
+++ server-project/src/protocol.h
@@ -1,24 +1,30 @@
 /*
  * protocol.h
  *
- * Server header file
- * Definitions, constants and function prototypes for the server
+ * Client header file
+ * Definitions, constants and function prototypes for the client
  */
 
 #ifndef PROTOCOL_H_
 #define PROTOCOL_H_
 
 // Shared application parameters
-#define SERVER_PORT 27015  // Server port (change if needed)
+#define SERVER_PORT 56700  // Server port (change if needed)
 #define BUFFER_SIZE 512    // Buffer size for messages
-#define QUEUE_SIZE 5       // Size of pending connections queue
+#define DEFAULT_IP "127.0.0.1"
 
-// Function prototypes
-// Add here the signatures of the functions implemented by students
+typedef struct {
+    char type;        // Weather data type: 't', 'h', 'w', 'p'
+    char city[64];    // City name (null-terminated string)
+} weather_request_t;
 
-/*
- * Example function to implement:
- * int handle_client(int client_socket);
- */
+typedef struct {
+    unsigned int status;  // Response status code
+    char type;            // Echo of request type
+    float value;          // Weather data value
+} weather_response_t;
+
+void errorhandler(char *errorMessage);
+void clearwinsock();
 
 #endif /* PROTOCOL_H_ */
