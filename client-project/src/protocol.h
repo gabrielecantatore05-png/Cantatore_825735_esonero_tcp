bce66912208111dcaad8b9311c9741a1f09f436b
gabrielecantatore05-png <gabriele.cantatore05@gmail.com>
2025-11-26 15:43:00 +0100

    Implementazione logica client-server TCP

--- client-project/src/protocol.h
+++ client-project/src/protocol.h
@@ -9,15 +9,22 @@
 #define PROTOCOL_H_
 
 // Shared application parameters
-#define SERVER_PORT 27015  // Server port (change if needed)
+#define SERVER_PORT 56700  // Server port (change if needed)
 #define BUFFER_SIZE 512    // Buffer size for messages
+#define DEFAULT_IP "127.0.0.1"
 
-// Function prototypes
-// Add here the signatures of the functions implemented by students
+typedef struct {
+    char type;        // Weather data type: 't', 'h', 'w', 'p'
+    char city[64];    // City name (null-terminated string)
+} weather_request_t;
 
-/*
- * Example function to implement:
- * int connect_to_server(const char* server_address);
- */
+typedef struct {
+    unsigned int status;  // Response status code
+    char type;            // Echo of request type
+    float value;          // Weather data value
+} weather_response_t;
+
+void errorhandler(char *errorMessage);
+void clearwinsock();
 
 #endif /* PROTOCOL_H_ */
