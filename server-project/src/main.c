a0b8d1b8f3b14b9370255f4c935bb25b52a6f6ad
gabrielecantatore05-png <gabriele.cantatore05@gmail.com>
2025-12-30 21:01:15 +0100

    Implementazione logica client-server TCP

--- server-project/src/main.c
+++ server-project/src/main.c
@@ -14,11 +14,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
 #include "protocol.h"
 
-#define NO_ERROR 0
+// --- Password Generation Functions (Requirements) ---
 
-// Implementation of required generation functions
 void generate_numeric(int len, char* out) {
     const char charset[] = "0123456789";
     for (int i = 0; i < len; i++) out[i] = charset[rand() % 10];
@@ -50,86 +50,107 @@ void clearwinsock() {
 #endif
 }
 
+void errorhandler(char *errorMessage) {
+    printf("%s", errorMessage);
+}
+
 int main(int argc, char *argv[]) {
     srand((unsigned int)time(NULL));
 
 #if defined WIN32
     WSADATA wsa_data;
-    if (WSAStartup(MAKEWORD(2,2), &wsa_data) != NO_ERROR) {
+    if (WSAStartup(MAKEWORD(2,2), &wsa_data) != 0) {
         printf("Error at WSAStartup()\n");
-        return -1;
+        return 0;
     }
 #endif
 
-    int my_socket = socket(AF_INET, SOCK_STREAM, 0);
+    // Create welcome socket
+    int my_socket;
+    my_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (my_socket < 0) {
-        printf("Socket creation failed.\n");
+        errorhandler("socket creation failed.\n");
         clearwinsock();
         return -1;
     }
 
-    struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(SERVER_PORT);
-    server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDRESS);
+    // Set connection settings
+    struct sockaddr_in sad;
+    memset(&sad, 0, sizeof(sad));
+    sad.sin_family = AF_INET;
+    sad.sin_addr.s_addr = inet_addr(SERVER_ADDRESS);
+    sad.sin_port = htons(PROTO_PORT);
 
-    if (bind(my_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        printf("Bind failed.\n");
+    if (bind(my_socket, (struct sockaddr*) &sad, sizeof(sad)) < 0) {
+        errorhandler("bind() failed.\n");
         closesocket(my_socket);
         clearwinsock();
         return -1;
     }
 
+    // Point 8: Listen with QLEN (5)
     if (listen(my_socket, QLEN) < 0) {
-        printf("Listen failed.\n");
+        errorhandler("listen() failed.\n");
         closesocket(my_socket);
         clearwinsock();
         return -1;
     }
 
-    printf("Server listening on %s:%d\n", SERVER_ADDRESS, SERVER_PORT);
+    struct sockaddr_in cad;
+    int client_socket;
+    int client_len;
+
+    printf("Server started. Waiting for connections on %s:%d...\n", SERVER_ADDRESS, PROTO_PORT);
 
     while (1) {
-        struct sockaddr_in client_addr;
-        int client_len = sizeof(client_addr);
-        int client_socket = accept(my_socket, (struct sockaddr*)&client_addr, &client_len);
+        client_len = sizeof(cad);
+        if ((client_socket = accept(my_socket, (struct sockaddr*) &cad, &client_len)) < 0) {
+            errorhandler("accept() failed.\n");
+            continue;
+        }
 
-        if (client_socket >= 0) {
-            // Point 2: Specific output format
-            printf("New connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+        // Point 2: Specific output format required
+        printf("New connection from %s:%d\n", inet_ntoa(cad.sin_addr), ntohs(cad.sin_port));
 
-            char buffer[BUFFER_SIZE];
-            while (1) {
-                memset(buffer, 0, BUFFER_SIZE);
-                int bytes = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
-                
-                // Point 7: Handle disconnection or 'q'
-                if (bytes <= 0 || buffer[0] == 'q') break;
+        char buffer[BUFFER_SIZE];
+        while (1) {
+            memset(buffer, '\0', BUFFER_SIZE);
+            int bytes_received = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
 
-                char type;
-                int length;
-                char password[MAX_PASS_LEN + 1];
+            // Point 7: Check for disconnection or 'q'
+            if (bytes_received <= 0 || buffer[0] == 'q') {
+                break;
+            }
 
-                // Point 4: Parse request and generate
-                if (sscanf(buffer, "%c %d", &type, &length) == 2) {
-                    if (length >= MIN_PASS_LEN && length <= MAX_PASS_LEN) {
-                        switch (type) {
-                            case 'n': generate_numeric(length, password); break;
-                            case 'a': generate_alpha(length, password); break;
-                            case 'm': generate_mixed(length, password); break;
-                            case 's': generate_secure(length, password); break;
-                            default: strcpy(password, "Invalid type"); break;
-                        }
-                        send(client_socket, password, strlen(password), 0);
-                    } else {
-                        char* err = "Length must be between 6 and 32";
-                        send(client_socket, err, strlen(err), 0);
+            char type;
+            int length;
+            char password[MAX_PASS_LEN + 1];
+
+            // Point 4: Parse request "type length"
+            if (sscanf(buffer, "%c %d", &type, &length) == 2) {
+                // Check requirements (length 6-32)
+                if (length >= MIN_PASS_LEN && length <= MAX_PASS_LEN) {
+                    switch (type) {
+                        case 'n': generate_numeric(length, password); break;
+                        case 'a': generate_alpha(length, password); break;
+                        case 'm': generate_mixed(length, password); break;
+                        case 's': generate_secure(length, password); break;
+                        default: strcpy(password, "Error: invalid type"); break;
                     }
+                    // Send generated password back to client
+                    send(client_socket, password, strlen(password), 0);
+                } else {
+                    char* msg = "Error: invalid length (6-32)";
+                    send(client_socket, msg, strlen(msg), 0);
                 }
             }
-            closesocket(client_socket);
         }
+
+        closesocket(client_socket);
+        printf("Connection closed.\n");
     }
+
+    closesocket(my_socket);
+    clearwinsock();
     return 0;
 }
