f0c280689388f6184a2faacdf50ed2f2fe00c07e
gabrielecantatore05-png <gabriele.cantatore05@gmail.com>
2025-12-30 18:17:22 +0100

    Implementazione logica client-server TCP

--- client-project/src/main.c
+++ client-project/src/main.c
@@ -1,12 +1,3 @@
-/*
- * main.c
- *
- * TCP Client - Template for Computer Networks assignment
- *
- * This file contains the boilerplate code for a TCP client
- * portable across Windows, Linux and macOS.
- */
-
 #if defined WIN32
 #include <winsock.h>
 #else
@@ -22,126 +13,63 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include "protocol.h"
-
-void errorhandler(char *errorMessage) {
-    printf("%s\n", errorMessage);
-}
 
 void clearwinsock() {
 #if defined WIN32
-	WSACleanup();
+    WSACleanup();
 #endif
 }
 
 int main(int argc, char *argv[]) {
-
 #if defined WIN32
-	WSADATA wsa_data;
-	if (WSAStartup(MAKEWORD(2,2), &wsa_data) != 0) {
-		printf("Errore in WSAStartup()\n");
-		return 0;
-	}
+    WSADATA wsa_data;
+    if (WSAStartup(MAKEWORD(2,2), &wsa_data) != 0) return -1;
 #endif
 
-	int port = SERVER_PORT;
-	char ip[32] = DEFAULT_IP;
-	char request_string[128] = "";
-	int richiesta = 0;
+    int my_socket = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDRESS);
 
-	weather_request_t request;
-	memset(&request, 0, sizeof(request));
+    // Point 1: Automatic connection
+    if (connect(my_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        printf("Connection failed.\n");
+        clearwinsock();
+        return -1;
+    }
 
+    char input[BUFFER_SIZE];
+    char password[BUFFER_SIZE];
 
-	for (int i = 1; i < argc; i++) {
-		if (strcmp(argv[i], "-s") == 0 && i + 1 < argc) {
-			strncpy(ip, argv[++i], 31);
-		} else if (strcmp(argv[i], "-p") == 0 && i + 1 < argc) {
-			port = atoi(argv[++i]);
-		} else if (strcmp(argv[i], "-r") == 0 && i + 1 < argc) {
-			strncpy(request_string, argv[++i], 127);
-			richiesta = 1;
-		}
-	}
+    while (1) {
+        // Point 3 & 6: Single read from standard input (e.g., "n 8")
+        printf("Enter request (n/a/m/s length) or 'q' to quit: ");
+        if (fgets(input, BUFFER_SIZE, stdin) == NULL) break;
+        input[strcspn(input, "\n")] = 0;
 
-	if (!richiesta) {
-		printf("Uso: %s [-s server] [-p port] -r \"tipo città\"\n", argv[0]);
-		clearwinsock();
-		return -1;
-	}
+        // Point 7: Quit condition
+        if (strcmp(input, "q") == 0) {
+            send(my_socket, "q", 1, 0);
+            break;
+        }
 
+        // Point 4: Send request
+        send(my_socket, input, strlen(input), 0);
 
-	int i = 0;
-	while (request_string[i] == ' ' && request_string[i] != '\0') i++;
+        // Point 5: Read and display password
+        memset(password, 0, BUFFER_SIZE);
+        if (recv(my_socket, password, BUFFER_SIZE - 1, 0) > 0) {
+            printf("Password: %s\n", password);
+        } else {
+            break;
+        }
+    }
 
-	request.type = request_string[i];
-	i++;
-
-	while (request_string[i] == ' ' && request_string[i] != '\0') i++;
-
-	strncpy(request.city, &request_string[i], 63);
-
-
-	int c_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (c_socket < 0) {
-		errorhandler("Creazione del socket fallita.\n");
-		clearwinsock();
-		return -1;
-	}
-
-	struct sockaddr_in sad;
-	memset(&sad, 0, sizeof(sad));
-	sad.sin_family = AF_INET;
-	sad.sin_addr.s_addr = inet_addr(ip);
-	sad.sin_port = htons(port);
-
-	if (connect(c_socket, (struct sockaddr *)&sad, sizeof(sad)) < 0) {
-		errorhandler("Connessione fallita.\n");
-		closesocket(c_socket);
-		clearwinsock();
-		return -1;
-	}
-
-
-	if(send(c_socket, (char*)&request, sizeof(request), 0) != sizeof(request)) {
-		errorhandler("send() ha inviato un numero di byte diverso dall'atteso");
-		closesocket(c_socket);
-		clearwinsock();
-		return -1;
-	}
-
-
-	weather_response_t response;
-	if ((recv(c_socket, (char*)&response, sizeof(response), 0)) <= 0) {
-		errorhandler("recv() fallita o connessione chiusa prematuramente");
-		closesocket(c_socket);
-		clearwinsock();
-		return -1;
-	}
-
-
-	printf("Ricevuto risultato dal server ip %s. ", ip);
-
-	if (response.status == 0) {
-		request.city[0] = toupper(request.city[0]);
-		switch (response.type) {
-			case 't': printf("%s: Temperatura = %.1f°C\n", request.city, response.value); break;
-			case 'h': printf("%s: Umidità = %.1f%%\n", request.city, response.value); break;
-			case 'w': printf("%s: Vento = %.1f km/h\n", request.city, response.value); break;
-			case 'p': printf("%s: Pressione = %.1f hPa\n", request.city, response.value); break;
-			default:  printf("Tipo sconosciuto ricevuto.\n");
-		}
-	} else if (response.status == 1) {
-		printf("Città non disponibile\n");
-	} else if (response.status == 2) {
-		printf("Richiesta non valida\n");
-	} else {
-		printf("Errore sconosciuto\n");
-	}
-
-	closesocket(c_socket);
-
-	clearwinsock();
-	return 0;
+    closesocket(my_socket);
+    printf("Client terminated.\n"); // Point 7 requirement
+    clearwinsock();
+    return 0;
 }
--- client-project/src/protocol.h
+++ client-project/src/protocol.h
@@ -1,30 +1,27 @@
 /*
  * protocol.h
- *
- * Client header file
- * Definitions, constants and function prototypes for the client
+ * Application protocol shared between client and server.
  */
 
 #ifndef PROTOCOL_H_
 #define PROTOCOL_H_
 
-// Shared application parameters
-#define SERVER_PORT 56700  // Server port (change if needed)
-#define BUFFER_SIZE 512    // Buffer size for messages
-#define DEFAULT_IP "127.0.0.1"
+// Default connection parameters (Point 1 & Requirements)
+#define SERVER_PORT 27015
+#define SERVER_ADDRESS "127.0.0.1"
 
-typedef struct {
-    char type;        // Weather data type: 't', 'h', 'w', 'p'
-    char city[64];    // City name (null-terminated string)
-} weather_request_t;
+// Buffer and Queue settings (Point 8)
+#define BUFFER_SIZE 512
+#define QLEN 5
 
-typedef struct {
-    unsigned int status;  // Response status code
-    char type;            // Echo of request type
-    float value;          // Weather data value
-} weather_response_t;
+// Password length constraints (Requirements)
+#define MIN_PASS_LEN 6
+#define MAX_PASS_LEN 32
 
-void errorhandler(char *errorMessage);
-void clearwinsock();
+// Required generation functions (Requirements)
+void generate_numeric(int len, char* out);
+void generate_alpha(int len, char* out);
+void generate_mixed(int len, char* out);
+void generate_secure(int len, char* out);
 
 #endif /* PROTOCOL_H_ */
